c518f1243a0ac9b7aa596630e1ef3c91f3225c68
novianto778 <63406837+Novianto778@users.noreply.github.com>
2024-08-11 15:25:43 +0700

    feat: home and news metadat

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,7 +1,18 @@
 import MainLayout from "@/components/layouts/MainLayout";
+import { envClient } from "@/lib/env";
 import News from "@/pages/News";
 import { NewsParams } from "@/types/news.type";
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "News - Circle",
+  description: "Discover the latest news and updates at Circle.",
+  keywords: ["news", "updates", "circle"],
+  alternates: {
+    canonical: envClient.SITE_URL + "/news",
+  },
+};
 
 type Props = {
   searchParams: NewsParams;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
 import MainLayout from "@/components/layouts/MainLayout";
+import { envClient } from "@/lib/env";
 import Home from "@/pages/Home";
-import Image from "next/image";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  alternates: {
+    canonical: envClient.SITE_URL,
+  },
+};
 
 export default function HomePage() {
   return (
